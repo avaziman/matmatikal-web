9afa373deae64d033c2294e9a3fa818a298a8088
ISickMan <adam.vaizman@gmail.com>
2024-04-21 03:52:00 +0300

    tbc

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatIconModule],
+  imports: [CommonModule, RouterModule, MatIconModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
